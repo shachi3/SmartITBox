93cdd6c225057d9c81d0b47c0d6f1e1dd32ee8ff
shachi3 <shachii309@gmail.com>
2026-01-21 14:53:57 +0530

    upadting

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/SmartITBox.git/',
+  base: '/SmartITBox/',
   plugins: [
     react(),
     tailwindcss()
